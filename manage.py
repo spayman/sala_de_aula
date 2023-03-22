ddeca5bba7b4d34022baab8aa9ba2df121540562
Flávio Pinheiro <fpssouzza@gmail.com>
2023-03-22 08:13:59 -0300

    22/03/2023
    Teste

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+'''COMMENTARY'''
+
+
+
+
 import os
 import sys
-
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
